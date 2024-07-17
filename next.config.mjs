3cd63fd0f3161b7e47a424b8ef1f61e51a4459da
Damian Szalbierz <szalbierz.d.k@gmail.com>
2024-07-17 10:22:39 +0200

    Fix velite

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,24 @@
-const isDev = process.argv.indexOf('dev') !== -1
-const isBuild = process.argv.indexOf('build') !== -1
-if (!process.env.VELITE_STARTED && (isDev || isBuild)) {
-  process.env.VELITE_STARTED = '1'
-  const { build } = await import('velite')
-  await build({ watch: isDev, clean: !isDev })
-}
+import { build } from 'velite'
 
 /** @type {import('next').NextConfig} */
 export default {
-  // next config here...
+  // othor next config here...
+  webpack: config => {
+    config.plugins.push(new VeliteWebpackPlugin())
+    return config
+  }
+}
+
+class VeliteWebpackPlugin {
+  static started = false
+  apply(/** @type {import('webpack').Compiler} */ compiler) {
+    // executed three times in nextjs
+    // twice for the server (nodejs / edge runtime) and once for the client
+    compiler.hooks.beforeCompile.tapPromise('VeliteWebpackPlugin', async () => {
+      if (VeliteWebpackPlugin.started) return
+      VeliteWebpackPlugin.started = true
+      const dev = compiler.options.mode === 'development'
+      await build({ watch: dev, clean: !dev })
+    })
+  }
 }
--- velite.config.ts
+++ velite.config.ts
@@ -1,7 +1,4 @@
 import { defineConfig, defineCollection, s } from "velite";
-// import rehypeSlug from "rehype-slug";
-// import rehypePrettyCode from "rehype-pretty-code";
-// import rehypeAutolinkHeadings from "rehype-autolink-headings";
 
 const computedFields = <T extends { slug: string }>(data: T) => ({
   ...data,
@@ -18,7 +15,6 @@ const posts = defineCollection({
       description: s.string().max(999).optional(),
       date: s.isodate(),
       published: s.boolean().default(true),
-      tags: s.array(s.string()).optional(),
       body: s.mdx(),
     })
     .transform(computedFields),
@@ -35,20 +31,7 @@ export default defineConfig({
   },
   collections: { posts },
   mdx: {
-    rehypePlugins: [
-      // rehypeSlug,
-      // [rehypePrettyCode, { theme: "github-dark" }],
-      // [
-      //   rehypeAutolinkHeadings,
-      //   {
-      //     behavior: "wrap",
-      //     properties: {
-      //       className: ["subheading-anchor"],
-      //       ariaLabel: "Link to section",
-      //     },
-      //   },
-      // ],
-    ],
+    rehypePlugins: [],
     remarkPlugins: [],
   },
 });

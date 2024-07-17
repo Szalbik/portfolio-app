3d32261e0c44178789f90bc5f754285a69ba80d2
Damian Szalbierz <szalbierz.d.k@gmail.com>
2024-07-17 10:22:46 +0200

    Setup blog page

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,23 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
+import type { Post } from "#site/content"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function formatDate(input: string | number): string {
+  const date = new Date(input)
+
+  return date.toLocaleDateString('en-US', {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  })
+}
+
+export function sortPosts(posts: Array<Post>) {
+  return posts.sort((a, b) => {
+    return new Date(b.date).getTime() - new Date(a.date).getTime()
+  })
 }
